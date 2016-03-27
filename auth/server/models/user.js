1089544fa8a96f49038b0966e68ce697eb6bcf45
Matthew Styers <mattysty@gmail.com>
2016-03-26 22:34:57 -0500

    adds missing comma

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 // Define our model
 const userSchema = new Schema({
-  email: { type: String, unique: true, lowercase: true }
+  email: { type: String, unique: true, lowercase: true },
   password: String
 });
 
