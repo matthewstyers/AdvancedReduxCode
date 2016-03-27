91fc6b740777a3c44f8eddf2be9a0953f0c3ba6e
Matthew Styers <mattysty@gmail.com>
2016-03-27 18:39:03 -0500

    adds mongo connection

--- auth/server/index.js
+++ auth/server/index.js
@@ -4,6 +4,10 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 const app = express();
 // App Setup
