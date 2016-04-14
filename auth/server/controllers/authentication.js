99701216faf52af0628be78b2f5a830e3de32ef0
Matthew Styers <mattysty@gmail.com>
2016-04-13 19:53:14 -0500

    adds controller functionality

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -5,10 +5,31 @@ exports.signup = function(req, res, next) {
   // See if a user with a given email exists
   User.findOne({
     email: email
-  }, function(err, existingUser) {});
-  // If a user with email does exist, return an error
+  }, function(err, existingUser) {
+    if (err) {
+      return next(err);
+    }
 
-  // If a user with email does NOT exist, create and save user record
+    // If a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({
+        error: 'Email is in use'
+      });
 
-// Respond to request indicating the user was created
+    // If a user with email does NOT exist, create and save user record
+    }
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+
+      // Respond to request indicating the user was created
+      res.json(user);
+    });
+  });
 }
