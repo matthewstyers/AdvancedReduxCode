8e4f982036c9a082c2f97d92089c13388c044b37
Matthew Styers <mattysty@gmail.com>
2016-04-13 20:40:21 -0500

    adds passport handler

--- auth/server/services/passport.js
+++ auth/server/services/passport.js
@@ -3,3 +3,26 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Setup options for JWT Strategy
+const jwtOptions = {};
+
+// Create JWT Strategy;
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  // see if the user ID in the payload exists in our database
+
+  // if if does, call 'done' with that user
+  // otherwise, call done without a user object
+  User.findByID(payload.sub, function(err, user) {
+    if (err) {
+      return done(err, false);
+    }
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+// Tell passport to use this strategy
